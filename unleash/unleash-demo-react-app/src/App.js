87194d736ff311e22f7001e8aaec87fbaba0d3f5
jhillierdavis <jhillierdavis@users.noreply.github.com>
2022-10-16 00:31:18 +0100

    Updated default ReactJS App. for Unleash provider and feature flag controlled header

--- unleash/unleash-demo-react-app/src/App.js
+++ unleash/unleash-demo-react-app/src/App.js
@@ -1,10 +1,28 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/header'
+
+import { FlagProvider } from '@unleash/proxy-client-react';
+
+const config = {
+  // url: 'https://PROXY_HOSTNAME/api/proxy', // or https://UNLEASH_HOSTNAME/api/frontend
+  url: 'http://localhost:4242/api/proxy',
+  // clientKey: 'PROXYKEY',
+  clientKey: 'default:development.unleash-insecure-api-token',
+  refreshInterval: 15,
+  appName: 'your-app-name',
+  environment: 'dev',
+};
+
 function App() {
+
   return (
+    <FlagProvider config={config}>
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +37,7 @@ function App() {
         </a>
       </header>
     </div>
+    </FlagProvider>
   );
 }
 
