891ca8c84aff81301f617ae1a310754afddcbf8a
jhillierdavis <jhillierdavis@users.noreply.github.com>
2022-10-15 17:28:53 +0100

    WIP update

--- growth-book/growth-book-demo-react-app/src/App.js
+++ growth-book/growth-book-demo-react-app/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import logo from './logo.svg';
-import './App.css';
 
-// import { GrowthBook, GrowthBookProvider, useFeature } from "@growthbook/growthbook-react";
-import { GrowthBook, GrowthBookProvider } from "@growthbook/growthbook-react";
+import { GrowthBook, GrowthBookProvider, useFeature } from "@growthbook/growthbook-react";
 import { useEffect } from "react";
-
-import gblogo from './growth-book-logo.png'
+import growthBookLogo from './growth-book-logo.png'
 
 // Create a GrowthBook instance
-
 const growthbook = new GrowthBook({
   trackingCallback: (experiment, result) => {
     console.log({
@@ -19,50 +15,38 @@ const growthbook = new GrowthBook({
   }
 });
 
-// Load feature definitions from API
-// In production, we recommend putting a CDN in front of the API endpoint
-const FEATURES_ENDPOINT = "http://localhost:3100/api/features/key_prod_9319e6633ec1f112";
-
-/*
-fetch(FEATURES_ENDPOINT)
-  .then((res) => res.json())
-  .then((json) => {
-    growthbook.setFeatures(json.features);
-
-    if (growthbook.isOn("imageswitch")) {
-      console.log("Feature 'imageSwitch' is On")
-    } else {
-      console.log("Feature 'imageSwitch' is Off")
-    }
-  })
-  .catch(() => {
-    console.log("Failed to fetch feature definitions from GrowthBook");
-  });
-
-// TODO: replace with real targeting attributes
-growthbook.setAttributes({
-    "id": "foo",
-    "deviceId": "foo",
-    "company": "foo",
-    "loggedIn": true,
-    "employee": true,
-    "country": "foo",
-    "browser": "foo",
-    "url": "foo"
-  });
-*/
-/*
 function App() {
+  useEffect(() => {
+    // Load feature definitions from API
+    // In production, we recommend putting a CDN in front of the API endpoint
+    fetch("http://localhost:3100/api/features/key_prod_144d3a5d0a19dc02")
+      .then((res) => res.json())
+      .then((json) => {
+        growthbook.setFeatures(json.features);
+      });
+
+    // TODO: replace with real targeting attributes
+    growthbook.setAttributes({
+      "id": "foo",
+      "deviceId": "foo",
+      "company": "foo",
+      "loggedIn": true,
+      "employee": true,
+      "country": "foo",
+      "browser": "foo",
+      "url": "foo"
+    })
+
+
+  }, [])
+
   return (
+    <GrowthBookProvider growthbook={growthbook}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {
-          // Use GB feature flag to switch between logos (GB logo & default)
-          imageSwitch ? ( <img src={gblogo} className="App-logo" alt="logo" /> ) : ( <img src={logo} className="App-logo" alt="logo" /> )
-        }
+      <header className="App-header">              
+          <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           TODO
         </p>
         <a
           className="App-link"
@@ -74,54 +58,8 @@ function App() {
         </a>
       </header>
     </div>
+    </GrowthBookProvider>
   );
 }
 
 export default App;
-*/
-
-export default function App() {
-  useEffect(() => {
-    // Load feature definitions from GrowthBook API
-    fetch(FEATURES_ENDPOINT)
-      .then((res) => res.json())
-      .then((parsed) => {
-        growthbook.setFeatures(parsed.features);
-        if (growthbook.isOn("imageswitch")) {
-          console.log("Feature 'imageSwitch' is On")
-        } else {
-          console.log("Feature 'imageSwitch' is Off")
-        }
-      });
-
-    // Set user attributes for targeting (from cookie, auth system, etc.)
-    growthbook.setAttributes({
-      // id: "123",
-      // company: "acme",
-    });
-  }, []);
-
-  return (
-    <GrowthBookProvider growthbook={growthbook}>
-      <div className="App">
-      <header className="App-header">        
-        {
-          // Use GB feature flag to switch between logos (GB logo & default)
-          growthbook.isOn("imageswitch") ? ( <img src={gblogo} className="App-logo" alt="logo" /> ) : ( <img src={logo} className="App-logo" alt="logo" /> )
-        }
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </div>
-    </GrowthBookProvider>
-  );
-}
