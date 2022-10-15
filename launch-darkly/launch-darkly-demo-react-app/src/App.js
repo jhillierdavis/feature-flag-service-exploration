e54ad2bc948070d051fa3cfaf95f0a175d318d99
jhillierdavis <jhillierdavis@users.noreply.github.com>
2022-10-15 15:13:52 +0100

    Added new Launch Darkly string feature flag 'bottom-text-enabled' to toggle bottom text to be displayed

--- launch-darkly/launch-darkly-demo-react-app/src/App.js
+++ launch-darkly/launch-darkly-demo-react-app/src/App.js
@@ -9,17 +9,17 @@ import { withLDProvider, useFlags } from 'launchdarkly-react-client-sdk'
 import ldlogo from './launch-darkly-logo.png'
 
 function App() {
-  const { logoSwitchEnabled } = useFlags()
+  const { logoSwitchEnabled, bottomTextEnabled } = useFlags()
 
   return (
     <div className="App">
       <header className="App-header">
         {
-          // Use LD feature flag to switch between logos (LD logo or default ReactJS logo)
+          // Use LD feature flag to switch between logos (LD logo or default ReactJS logox)
           logoSwitchEnabled ? ( <img src={ldlogo} className="App-logo" alt="logo" /> ) : ( <img src={logo} className="App-logo" alt="logo" /> )
         }
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           { bottomTextEnabled }
         </p>
         <a
           className="App-link"
