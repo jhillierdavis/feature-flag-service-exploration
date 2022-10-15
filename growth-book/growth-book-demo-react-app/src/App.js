7e422593d5c8ef7e471d09be947fa51bf6835f9f
jhillierdavis <jhillierdavis@users.noreply.github.com>
2022-10-15 17:41:50 +0100

    Reinstated CSS import

--- growth-book/growth-book-demo-react-app/src/App.js
+++ growth-book/growth-book-demo-react-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import logo from './logo.svg';
+import logo from './logo.svg'
+import './App.css'
 
 import { GrowthBook, GrowthBookProvider, useFeature } from "@growthbook/growthbook-react";
 import { useEffect } from "react";
@@ -62,4 +63,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
