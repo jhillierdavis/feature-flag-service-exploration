4e3a047e4b2cb754870535126951b408106b7bb0
jhillierdavis <jhillierdavis@users.noreply.github.com>
2022-10-16 17:01:26 +0100

    Added convenience script to check Proxy API

---
+++ unleash/unleash-demo-react-app/curl-check-proxy-health.sh
@@ -0,0 +1,1 @@
+curl http://localhost:3000/proxy/health -I
---
+++ unleash/unleash-demo-react-app/curl-check-proxy-toggles.sh
@@ -0,0 +1,4 @@
+curl -H "Content-Type: application/json" \
+     -H "Authorization: proxy-client-key" \
+     -X GET \
+     http://localhost:3000/proxy
